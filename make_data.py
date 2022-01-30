66ad0f0815d05026f8dc7029f28e634b961f9914
chocotiamo <87431209+tiamo405@users.noreply.github.com>
2022-01-30 22:03:17 +0700

    Update make_data.py

--- make_data.py
+++ make_data.py
@@ -10,44 +10,65 @@ chương trình sẽ tự chụp ảnh nên bạn chỉ cần giơ tay theo đú
 Rất cảm ơn vì đã giúp mình
 Choco Tiamo <3
 '''
-'''
-nó bị overfit, nên tôi đã bỏ mấy dòng để hiện ảnh nhỏ nhỏ đi, k biết có phải do nó không nữa.
-'''
 import cv2
 import os
-labels=["0","1","2","3","4","5"]
-#labels=["9"]
-name="Nam1"
+if not os.path.exists('data/') : 
+    os.mkdir('data/')
+labels=["0","1","2","3","4"]
+name="Nam"
 for label in labels :
     print("make_data so ", label)
     i=0
     cap=cv2.VideoCapture(0)
-    #image_hand=cv2.imread("image_hand\\"+label+'.png')
     while(True):
         i+=1
         ret,frame =cap.read()
         if not ret :
             continue
         frame =cv2.resize(frame,dsize=None,fx=0.5,fy=0.5)
-
-        # cho 1 ảnh lên camera, ảnh có 3 chiều nha
-        #h, w, c=image_hand.shape
-        #frame[0: h, 0: w]=image_hand
         cv2.imshow('camera',frame)
 
         if i >=100 :
             
             j=int(i-100)
             print("So anh capture" , j )
-            # tạo folder train nếu chưa có
-            if not os.path.exists('train/') : 
-                os.mkdir('train/')
-            # lưu ảnh trên camera
-            cv2.imwrite('train/' + name + '.' + label + '.' + str(j) + '.png', frame)
-        if cv2.waitKey(1) & 0xFF == ord ('q'):
-            break
+            if not os.path.exists('data/train') : 
+                os.mkdir('data/train')
+            if not os.path.exists('data/train'+'/'+label) : 
+                os.mkdir('data/train'+'/'+label)
+            cv2.imwrite('data/train/'+label + '/' + name + '.' + label + '.' + str(j) + '.png', frame)
         if i>=299 :
+            break
+        if cv2.waitKey(1) & 0xFF == 27: #ấn ESC để out
+            break
+
+labels=["0","1","2","3","4"]
+name="Nam"
+for label in labels :
+    print("make_data so ", label)
+    i=0
+    cap=cv2.VideoCapture(0)
+    while(True):
+        i+=1
+        ret,frame =cap.read()
+        if not ret :
+            continue
+        frame =cv2.resize(frame,dsize=None,fx=0.5,fy=0.5)
+        cv2.imshow('camera',frame)
+
+        if i >=100 :
+            
+            j=int(i-100)
+            print("So anh capture" , j )
+
+            if not os.path.exists('data/valid') : 
+                os.mkdir('data/valid')
+            if not os.path.exists('data/valid'+'/'+label) : 
+                os.mkdir('data/valid'+'/'+label)
+            cv2.imwrite('data/valid/'+label + '/' + name + '.' + label + '.' + str(j) + '.png', frame)
+        if i>=145 :
+            break
+        if cv2.waitKey(1) & 0xFF == 27: #ấn ESC để out
             break
 cap.release()
 cv2.destroyAllWindows()
-
