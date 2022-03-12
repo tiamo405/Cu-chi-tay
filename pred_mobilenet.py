1ed63736109e1063961be5d5137dd17255800023
chocotiamo <87431209+tiamo405@users.noreply.github.com>
2022-03-12 20:32:56 +0700

    Update and rename test_torch.py to pred_mobilenet.py

--- pred_mobilenet.py
+++ pred_mobilenet.py
@@ -1,5 +1,6 @@
 from re import I
 import numpy as np
+
 import torch
 import torch.nn as nn
 import torch.optim as optim
@@ -9,22 +10,25 @@ import cv2
 import time
 import torchvision.models as models
 
-batch_size = 32
-LR = 1e-3 
-epochs = 10
 width=224
 height=224
+print(torch.__version__ )
+
 
 class MobileNetv2(nn.Module):
-    def __init__(self) -> None:
+    def __init__(self, mode="train") -> None:
         super(MobileNetv2, self).__init__()
+        self.mode = mode
         self.mobilenetv2 = models.mobilenet_v2(pretrained=True)
         self.mobilenetv2.classifier[1] = nn.Linear(1280, 5)
         self.softmax = nn.Sequential(nn.Softmax(dim=1))
 
     def forward(self, x):
-      x = self.mobilenetv2(x)
-      return x
+        if self.mode == "train":
+            x = self.mobilenetv2(x)
+        elif self.mode == "pred":
+            x = self.softmax(self.mobilenetv2(x))
+        return x
 
 def image_transform(img):
   transform = transforms.Compose([
@@ -33,7 +37,6 @@ def image_transform(img):
   ])
   img = transform(img)
   return img
-
 def predict(PATH_img):
     img=cv2.imread(PATH_img)
     img = cv2.resize(img, (width, height))
@@ -62,28 +65,23 @@ while True :
     if not ret :
         continue
     frame =cv2.resize(frame,dsize=None,fx=0.5,fy=0.5)
-    cv2.imshow('camera',frame)
-    
+
     print("Giữ nguyên tư thế tay để chụp")
 
-    #time.sleep(10)
-    time_1= time.time()
-    if time_1 - time_2 >=5 :
-        i+=1
-        cv2.imwrite('test_camera/' + 'camera' + '.' + str(i) + '.png', frame)
-        print("đã chụp được ảnh")
-        path_img='test_camera\\' + 'camera' + '.' + str(i)+ '.png'
-        print('path_img : ',path_img)
-        res=predict(path_img)
-        print('Số dự đoán là :',res)
-        ans.append(res)
+    cv2.imwrite('test_camera/' + 'camera' + '.' + str(i) + '.png', frame)
+    print("đã chụp được ảnh")
+    path_img='test_camera\\' + 'camera' + '.' + str(i)+ '.png'
+    print('path_img : ',path_img)
+    res=predict(path_img)
+    print('Số dự đoán là :',res)
+    ans.append(res)
 
-        cv2.putText(frame,f"res index {i} : {int(res)}",(10,20), 
-        cv2.FONT_HERSHEY_PLAIN,2,(255,0,0),3)
-        cv2.imshow('res',frame)
-        time_2 = time_1
+    cv2.putText(frame,f"res index {i} : {int(res)}",(10,20), 
+    cv2.FONT_HERSHEY_PLAIN,2,(255,0,0),3)
+    cv2.imshow('res',frame)
     if cv2.waitKey(1) & 0xFF == 27: #ESC để out
         break
+    #cv2.imshow('camera',frame)
 cap.release()
 cv2.destroyAllWindows()
 print(ans)
